538b0dfb896aed6a7e2aa04e330a07a376c50747
olivier <olivier.grenier5@gmail.com>
2023-12-10 00:49:55 -0500

    completed 3 and 4

--- Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.cpp
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.cpp
@@ -6,3 +6,33 @@ void UBobAndAliceGameSubsystem::Initialize(
 }
 
 void UBobAndAliceGameSubsystem::Deinitialize() { Super::Deinitialize(); }
+
+void UBobAndAliceGameSubsystem::SaveTimeToSubsystem(const float time)
+{
+	this->timeLeft = time;
+}
+
+void UBobAndAliceGameSubsystem::SaveIsPickedUpToSubsystem(const bool pickedUp)
+{
+	this->pickedUpPackage = pickedUp;
+}
+
+void UBobAndAliceGameSubsystem::SaveIsDeliveredToSubsystem(const bool delivered)
+{
+	this->deliveredPackage = delivered;
+}
+
+float UBobAndAliceGameSubsystem::GetLocalTimeLeft() const
+{
+	return this->timeLeft;
+}
+
+bool UBobAndAliceGameSubsystem::GetLocalPickedUpPackage() const
+{
+	return this->pickedUpPackage;
+}
+
+bool UBobAndAliceGameSubsystem::GetLocalDeliveredPackage() const
+{
+	return this->deliveredPackage;
+}
--- Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.h
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.h
@@ -16,4 +16,27 @@ public:
 
   virtual void Deinitialize() override;
 
+  UFUNCTION(BlueprintCallable)
+  void SaveTimeToSubsystem(const float time);
+
+  UFUNCTION(BlueprintCallable)
+  void SaveIsPickedUpToSubsystem(const bool pickedUp);
+
+  UFUNCTION(BlueprintCallable)
+  void SaveIsDeliveredToSubsystem(const bool delivered);
+
+  UFUNCTION(BlueprintCallable, BlueprintPure)
+  float GetLocalTimeLeft() const;
+
+  UFUNCTION(BlueprintCallable, BlueprintPure)
+  bool GetLocalPickedUpPackage() const;
+
+  UFUNCTION(BlueprintCallable, BlueprintPure)
+  bool GetLocalDeliveredPackage() const;
+
+private:
+  int timeLeft;
+  bool pickedUpPackage;
+  bool deliveredPackage;
+
 };
